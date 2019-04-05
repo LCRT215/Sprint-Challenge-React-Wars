08c1ec95fa535efe48f9c4e8afadbf94f62c71ef
Leslie Thompson <lcrthompson@gmail.com>
2019-04-05 08:44:47 -0700

    setting up. Added character info file & connected to app.js

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,6 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Card from "./components/CharacterInfo";
 
 class App extends Component {
   constructor() {
@@ -10,7 +11,7 @@ class App extends Component {
   }
 
   componentDidMount() {
-    this.getCharacters('https://swapi.co/api/people/');
+    this.getCharacters("https://swapi.co/api/people/");
   }
 
   getCharacters = URL => {
---
+++ starwars/src/components/CharacterInfo.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./StarWars.css";
+
+//Setting up how the character info is displayed on the page.
+//Adding props so the data can be applied in App.js
+const Info = props => {};
+
+export default Info;
