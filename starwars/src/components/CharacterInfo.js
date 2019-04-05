e582aa6db75e0624a74f638e41adf61615aabc3b
Leslie Thompson <lcrthompson@gmail.com>
2019-04-05 08:55:04 -0700

    jsx added on character info

--- starwars/src/components/CharacterInfo.js
+++ starwars/src/components/CharacterInfo.js
@@ -3,6 +3,20 @@ import "./StarWars.css";
 
 //Setting up how the character info is displayed on the page.
 //Adding props so the data can be applied in App.js
-const Info = props => {};
+const CharacterInfo = props => {
+  return (
+    <div className="container">
+      <div className="card">
+        <h1>Name: </h1>
+        <p>Species: </p>
+        <p>Homeworld: </p>
+        <p>Birth Year:</p>
+        <p>Gender:</p>
+        <p>Height:</p>
+        <p>Mass:</p>
+      </div>
+    </div>
+  );
+};
 
-export default Info;
+export default CharacterInfo;
